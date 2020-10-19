63313e526d55f8bce6753cafe6da4a0c0854a63a
Muthualamelu <66001728+Muthualamelu@users.noreply.github.com>
2020-10-19 22:37:00 +0530

    Update calci.java

--- calci.java
+++ calci.java
@@ -1,2 +1,9 @@
-public static void main(String [] args){
+class Calc{
+  public static int add(int a,int b){
+    return a+b;
+  }
+  public static void main(String [] args){
+    add(10,20);
+  }
+}
 
